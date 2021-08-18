da038fa9c02f3d67d4d2769fa8e63a703ce1c678
ZTStory <m18740395237@163.com>
2021-08-18 14:39:04 +0800

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  publicDir: process.env.NODE_ENV === "production" ? '/vue_page_transition/' : '/',
+  plugins: [vue()],
+  build: {
+    outDir: "dist",
+  }
 })
